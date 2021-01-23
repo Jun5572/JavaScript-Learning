33abf3ab3a4b4a997e2b65d9b651fcf3894f8d4d
= <takizawa.tab@gmail.com>
2021-01-23 12:09:31 +0900

    改修中

--- callback-function/main2.js
+++ callback-function/main2.js
@@ -1,33 +1,64 @@
 let btn__follow = document.querySelector(".btn__follow");
 let btn__tweet = document.querySelector(".btn__tweet");
 let tweet_index = document.querySelector("#index");
-let tweets = [];
+let elm_input = document.querySelector("#tweet-content");
 let maxTweetLength = 10;
 
 btn__follow.addEventListener("click", function () {
   if (isFollowing()) {
     confirmed(unfollow);
   } else {
-    confirmed(follow);
+    follow();
   }
 });
 
-btn__tweet.addEventListener("click", function () {
-  let elm_input = document.querySelector("#tweet-content");
-  let tweet_text = elm_input.value;
-  let tweet_status = checkTweet(tweet_text);
-  
-  if (tweet_status.length && !tweet_status.blank) {
-    confirmed(postTweet, tweet_text);
-    clearForm(elm_input);
-  } else {
-    window.alert(`${maxTweetLength}文字以下で入力してください`);
-  }
+// btn__tweet.addEventListener("click", function () {
+//   let elm_input = document.querySelector("#tweet-content");
+//   let tweet_text = elm_input.value;
+//   let tweet_status = checkTweet(tweet_text);
 
-  if (tweet_status.blank) {
-    window.alert('入力してください');
-  }
+//   if (tweet_status.length && !tweet_status.blank) {
+//     confirmed(postTweet, tweet_text);
+//     clearForm(elm_input);
+//   } else {
+//     window.alert(`${maxTweetLength}文字以下で入力してください`);
+//   }
+
+//   if (tweet_status.blank) {
+//     window.alert('入力してください');
+//   }
+// });
+let tweet_text = elm_input.value;
+let tweet_status = checkTweet(tweet_text);
+
+btn__tweet.addEventListener("click", function(e) {
+  // getVal(e);
+
+  console.log(e);
+  // if (tweet_status.length && !tweet_status.blank) {
+  //   confirmed(postTweet, tweet_text);
+  //   clearForm(elm_input);
+  // } else {
+  //   window.alert(`${maxTweetLength}文字以下で入力してください`);
+  // }
+
+  // if (tweet_status.blank) {
+  //   window.alert("入力してください");
+  // }
 });
+
+function getVal(elm) {
+  return elm.value;
+}
+
+
+function isBlank(elm) {
+  return elm.value = "" ? true : false;
+}
+
+function isLengthOver(text, maxLimit) {
+  return text < maxLimit ? true : false;
+}
 
 //tweet可能か判定する関数
 function checkTweet(text) {
@@ -43,7 +74,7 @@ function postTweet(text) {
   let elm_list = document.createElement("li");
   elm_list.classList.add("body");
   elm_list.innerHTML = `<p>${text}</p>`;
-  tweet_index.appendChild(elm_list);
+  tweet_index.aïppendChild(elm_list);
 }
 
 //フォローしているかをチェックする関数。今回はクラスを持っているかで判定。
@@ -66,6 +97,7 @@ function unfollow() {
   console.log("フォローを外しました");
 }
 
+//実行前に確認をユーザーに対して行う関数
 function confirmed(fn, arg) {
   if (window.confirm("実行しますか？")) {
     fn(arg);
@@ -73,6 +105,7 @@ function confirmed(fn, arg) {
     alert("キャンセルしました");
   }
 }
+
 
 //引数に指定したクラスを持っているかの判定を行う関数
 //戻り値 Boolean
@@ -82,10 +115,6 @@ function checkClass(element, strClassName) {
 
 function clearForm(elm) {
   elm.value = "";
-}
-
-function isBlank(elm) {
-  return elm.value = '' ? true : false;
 }
 
 
